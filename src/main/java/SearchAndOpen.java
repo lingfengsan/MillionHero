98ccd81a190027e9c834e811ee184c0dcb567cc3
lingfengsan <cycllfs@gmail.com>
2018-01-11 10:30:48 +0800

    增加了对MAC打开网页的支持

--- src/main/java/SearchAndOpen.java
+++ src/main/java/SearchAndOpen.java
@@ -1,12 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.concurrent.Callable;
 
 /**
  * Created by 618 on 2018/1/8.
+ *
  * @author lingfengsan
  */
 public class SearchAndOpen implements Callable {
@@ -15,12 +19,40 @@ public class SearchAndOpen implements Callable {
     SearchAndOpen(String question) {
         this.question = question;
     }
+
     private Long searchAndOpen(String question) throws IOException {
-        String path = "http://www.baidu.com/s?tn=ichuner&lm=-1&word=" +
-                URLEncoder.encode(question, "gb2312") + "&rn=20";
-        Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + path);
+        String path = null;
+        try {
+            path = "http://www.baidu.com/s?tn=ichuner&lm=-1&word=" +
+                    URLEncoder.encode(question, "gb2312") + "&rn=20";
+            //获取操作系统的名字
+            String osName = System.getProperty("os.name", "");
+            if (osName.startsWith("Mac OS")) {
+                //苹果的打开方式
+                Class fileMgr = Class.forName("com.apple.eio.FileManager");
+                Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[]{String.class});
+                openURL.invoke(null, new Object[]{path});
+            } else if (osName.startsWith("Windows")) {
+                //windows的打开方式。
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + path);
+            }
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+//        Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + path);
         return new Search(question).search(question);
     }
+
     public static void main(String[] args) throws Exception {
         SearchAndOpen search = new SearchAndOpen("阿尔茨海默症又被称为什么?");
         System.out.println(search.call());
